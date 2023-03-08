ed8f160f118bf5ad8fca3b4a61518329b9d719b5
JoshMorrison99 <josh.morrison1999@gmail.com>
2023-03-08 14:54:51 -0500

    Update ssl-pinning.sh

--- ssl-pinning.sh
+++ ssl-pinning.sh
@@ -1,7 +1,7 @@
 # Downlaod frida-server
-wget https://github.com/frida/frida/releases/download/16.0.10/frida-server-16.0.10-android-arm64.xz
-xz -d frida-server-16.0.10-android-arm64.xz
-mv frida-server-16.0.10-android-arm64 frida-server
+wget https://github.com/frida/frida/releases/download/16.0.10/frida-server-16.0.10-android-x86.xz
+xz -d frida-server-16.0.10-android-x86.xz
+mv frida-server-16.0.10-android-x86 frida-server
 
 # connect to adb
 adb connect 192.168.31.103:5555
